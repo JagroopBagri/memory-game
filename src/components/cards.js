af8fa1cd6a857d02e3e8002a78eae743d2779df7
Jagroop <jagroopbagri5@gmail.com>
2022-03-31 16:40:48 -0700

    fixed bug in game that would prevent it from restarting

--- src/components/cards.js
+++ src/components/cards.js
@@ -4,14 +4,14 @@ function Cards(props) {
   // declare an array that will be used to identify whether a particular owl has been clicked
   let clickedArray = [];
   // function that fills the previously made array with 12 items that hold the boolean "false"
-  const fillClickArray = () => {
+  const fillClickArray = (array) => {
     for (let i = 1; i < 13; i++) {
-      clickedArray.push('false');
+      array.push('false');
     }
     return 0;
   };
   // invoke above function
-  fillClickArray();
+  fillClickArray(clickedArray);
   // use state that starts with the value of clickedArray
   const [clicked, setClicked] = React.useState(clickedArray);
   // function that will be used in onClick event listener to update user score when an owl is clicked
@@ -34,6 +34,14 @@ function Cards(props) {
       return a;
     });
   };
+  // function that will be used to reset the clicked array
+  const resetClicked = () => {
+    setClicked(() => {
+      let a = [];
+      fillClickArray(a);
+      return a;
+    });
+  };
   // declare an array that will be used to create "owl" elements
   let owlArray = [];
   // function that fills owl array with 12 items
@@ -47,12 +55,12 @@ function Cards(props) {
   fillOwlArray();
   // onClick event function
   const cardEvent = (e) => {
-    console.log(e.target);
     if (e.target.dataset.clicked === 'false') {
       updateScore();
-      e.target.dataset.clicked = 'true';
+      updateClicked(e);
     } else {
       resetScore();
+      resetClicked();
     }
   };
   // using map function to create react jsx elements
